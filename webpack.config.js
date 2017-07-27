6b6753d2060833e16e5116f5254927c56d849489
Kane Gregory <kane.gregory@ADAPTIVE.local>
2017-07-27 10:09:05 +0100

    adds the initial app structure

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: './src/gui/index.js',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Skreebo'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2016', 'react']
+                    }
+                }]
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'skreebo.js'
+    }
+};
+
+module.exports = config;
